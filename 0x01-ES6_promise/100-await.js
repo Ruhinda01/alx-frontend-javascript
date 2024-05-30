117b46f3ffd91b21d140ed01445a3fe1e8dafea2
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-05-30 06:29:18 +0300

    await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+
+  try {
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
