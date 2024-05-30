0f67842fd9acde6660097ab62e9cea0b1fa56194
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-05-30 05:40:33 +0300

    6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+  return Promise.allSettled([signUp, upload])
+    .then((results) => {
+      const arrData = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          arrData.push({ status: result.status, value: result.value });
+        } else {
+          arrData.push({ status: result.status, value: result.reason });
+        }
+      });
+      return arrData;
+    });
+}
